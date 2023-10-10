9a21f1991aebe53aebe70860b1104403ec017c2c
Sea-i <144750241+Sea-i@users.noreply.github.com>
2023-10-10 14:57:10 +0900

    Add files via upload

--- js/sea.js
+++ js/sea.js
@@ -2,10 +2,11 @@ $(document).on("pagecreate", "#layout" , function(){
     console.log("%c@kimseoha", "font-size:20px; color:white; background:#6667aa; font-family: 'InkLipquid'; font-weight : bold; padding:5px 10px;")
     
     //한 섹션 씩 스크롤 되게 하기 
-    const scStop = $("#layout .move");
+    const  winW = $(window).width();
+    const scStop = $("#container .move");
     
   $(scStop).each(function(){
-        if( $("#popup").css("display","none") ){ 
+        if( $("#big-content").css("display","none")  && winW > 820 ){ 
             $(this).on("mousewheel DOMMouseScroll", function(e){
                 e.preventDefault();
                 let delta = 0;
@@ -22,10 +23,13 @@ $(document).on("pagecreate", "#layout" , function(){
                         moveTop = $(this).next().offset().top;
                     }
                 }//end if 
-                    else  {
-                        if($(this).prev() !=undefined) {
-                            moveTop = $(this).prev().offset().top;
-                        };
+                else {    
+                    if($(this) == $("#home") ) {          
+                            moveTop = 0;    
+                        }
+                        else {
+                            moveTop = $(this).prev().offset(),top;      
+                        }
                     }//end else
     
                     $("html, body").stop().animate({ 
@@ -149,15 +153,9 @@ $(document).on("pagecreate", "#layout" , function(){
         }
     });
 
-    const layW = $("#layout").width();
     const homeTop = $("#home").offset().top;
     const aboutTop = $("#about").offset().top -100;
     let scrollTop = 0;
-
-    if( layW <= 1920 ){
-        // alret("이 웹페이지는  1920px 이상에서는 원활하지 않으니 브라우저의 사이즈를 줄여주세요");
-        // console.log ("왜 또 이상해졌냐고");
-    }
 
     // 스크롤에 반응하는 스크립트
     $(document).scroll(function(){
@@ -177,42 +175,85 @@ $(document).on("pagecreate", "#layout" , function(){
         }
     });//end window.scroll
 
-    // <web>이미지 슬라이드 우>좌
-    let slide = setInterval( webSlide , 2800 );
+    if ( winW > 1280 ){          
+        // <web>이미지 슬라이드 우>좌
+        let slide = setInterval( webSlide , 2800 );
 
-    function webSlide(){
-        $("#slideArea").stop().animate( { left : "-1400px"} , 2700, function(){
-            $("#slideArea").append( $("#slideArea").children().first() );
-            $("#slideArea").css( "left" , "0" );
+        function webSlide(){
+            $("#slideArea").stop().animate( { left : "-1400px"} , 2700, function(){
+                $("#slideArea").append( $("#slideArea").children().first() );
+                $("#slideArea").css( "left" , "0" );
+            });
+        }
+        $("#slideArea").hover(
+            function(){
+                clearInterval ( slide );
+            },function(){
+                slide = setInterval( webSlide , 2800 );
+            });
+
+                // 이벤트페이지, 웹이벤트 슬라이드 위 > 아래
+        let eSlide = setInterval ( evtSlide , 2800 );
+        
+        function evtSlide(){
+            $("#et-web-slide").stop().animate( { top : "-200px"} , 2500, function(){
+                $("#et-web-slide").prepend( $("#et-web-slide").children().last() );
+                $("#et-web-slide").css( "top" , "-575px" );
+            });    
+        }
+
+        $("#et-web-slide >div").hover(
+            function(){
+                clearInterval( eSlide );
+                $(this).children(".et-info").stop().fadeIn().css("display" , "flex");
+            }, 
+            function(){
+                eSlide = setInterval ( evtSlide , 2800 );
+                $(".et-info").stop().fadeOut();
+            });
+
+        // sns event slide 아래에서 위로
+        let sSlide = setInterval ( snsSlide , 2800 );
+        
+        function snsSlide(){
+            $("#et-sns-slide").stop().animate( { top : "-575px"} , 2500, function(){
+                $("#et-sns-slide").append( $("#et-sns-slide").children().first() );
+                $("#et-sns-slide").css( "top" , "-200px" );
+            });
+        }
+
+        $("#et-sns-slide >div").hover(
+            function(){
+                clearInterval( sSlide );
+                $(this).children(".et-info").fadeIn().css("display" , "flex");
+            }, 
+            function(){
+                sSlide = setInterval ( snsSlide , 2800 );
+                $(".et-info").fadeOut();
+            });
+    }
+    // 상세페이지 이미지에 마우스를 올리면, 큰 화면
+    
+    if(winW > 820 ){
+        $("#big").on("mouseenter focus",function(){
+            $("#dt-info").fadeIn();
+        });
+        $("#dt-info").mouseleave(function(){
+            $("#dt-info").fadeOut();
         });
     }
-
-    $("#slideArea").hover(
-        function(){
-            clearInterval ( slide );
-        },function(){
-            slide = setInterval( webSlide , 2800 );
+        $("#small button").on("mouseenter focus", function(){
+            const smallSrc = $(this).children().attr("src");
+            const smallAlt = $(this).children().attr("alt");
+            $("#big img").attr({ src : smallSrc , alt : smallAlt });
         });
-
-    // 상세페이지 이미지에 마우스를 올리면, 큰 화면
-    $("#big").on("mouseenter focus",function(){
-        $("#dt-info").fadeIn();
-    });
-    $("#dt-info").mouseleave(function(){
-        $("#dt-info").fadeOut();
-});
-    $("#small button").on("mouseenter focus", function(){
-        const smallSrc = $(this).children().attr("src");
-        const smallAlt = $(this).children().attr("alt");
-        $("#big img").attr({ src : smallSrc , alt : smallAlt });
-    });
-
+    
     // 큰 화면에서 자세히 보기를 누르면 원본 사진이 뜬다(디테일 디자인)
     $("#dt-info").click(function(){
-        const dtBigImgSrc = $(this).prev().prev().children().children().attr("src");
+        const dtBigImgSrc = $(this).prev().children().attr("src");
         const dtBigImgSrcChan = dtBigImgSrc.replace( ".jpg" , "-big.jpg" );
-        const dtBigImgAlt = $(this).prev().prev().children().children().attr("alt");
-
+        const dtBigImgAlt = $(this).prev().children().attr("alt");
+    
         $("#side").hide();
         $("#big-content").fadeIn();
         $("#big-content h4").text( dtBigImgAlt );
@@ -248,44 +289,62 @@ $(document).on("pagecreate", "#layout" , function(){
         $("#side").css("display","flex");
     });
     
-    // 이벤트페이지, 웹이벤트 슬라이드 위 > 아래
-    let eSlide = setInterval ( evtSlide , 2800 );
-    
-    function evtSlide(){
-        $("#et-web-slide").stop().animate( { top : "-200px"} , 2500, function(){
-            $("#et-web-slide").prepend( $("#et-web-slide").children().last() );
-            $("#et-web-slide").css( "top" , "-575px" );
-        });    
-    }
+    if( winW <= 1280 && winW > 820 ){
+        // <web>이미지 슬라이드 우>좌
+        let slide = setInterval( webSlide , 2800 );
 
-    $("#et-web-slide >div").hover(
-        function(){
-            clearInterval( eSlide );
-            $(this).children(".et-info").stop().fadeIn().css("display" , "flex");
-        }, 
-        function(){
-            eSlide = setInterval ( evtSlide , 2800 );
-            $(".et-info").stop().fadeOut();
-        });
+        function webSlide(){
+            $("#slideArea").stop().animate( { left : "-1000px"} , 2700, function(){
+                $("#slideArea").append( $("#slideArea").children().first() );
+                $("#slideArea").css( "left" , "0" );
+            });
+        }
+        $("#slideArea").hover(
+            function(){
+                clearInterval ( slide );
+            },function(){
+                slide = setInterval( webSlide , 2800 );
+            });
 
-    // sns event slide 아래에서 위로
-    let sSlide = setInterval ( snsSlide , 2800 );
-    
-    function snsSlide(){
-        $("#et-sns-slide").stop().animate( { top : "-575px"} , 2500, function(){
-            $("#et-sns-slide").append( $("#et-sns-slide").children().first() );
-            $("#et-sns-slide").css( "top" , "-200px" );
-        });
-    }
+                // 이벤트페이지, 웹이벤트 슬라이드 위 > 아래
+        let eSlide = setInterval ( evtSlide , 2800 );
 
-    $("#et-sns-slide >div").hover(
-        function(){
-            clearInterval( sSlide );
-            $(this).children(".et-info").fadeIn().css("display" , "flex");
-        }, 
-        function(){
-            sSlide = setInterval ( snsSlide , 2800 );
-            $(".et-info").fadeOut();
-        });
+        function evtSlide(){
+            $("#et-web-slide").stop().animate( { top : "-175px"} , 2500, function(){
+                $("#et-web-slide").prepend( $("#et-web-slide").children().last() );
+                $("#et-web-slide").css( "top" , "-500px" );
+            });    
+        }
+
+        $("#et-web-slide >div").hover(
+            function(){
+                clearInterval( eSlide );
+                $(this).children(".et-info").stop().fadeIn().css("display" , "flex");
+            }, 
+            function(){
+                eSlide = setInterval ( evtSlide , 2800 );
+                $(".et-info").stop().fadeOut();
+            });
+
+        // sns event slide 아래에서 위로
+        let sSlide = setInterval ( snsSlide , 2800 );
+
+        function snsSlide(){
+            $("#et-sns-slide").stop().animate( { top : "-500px"} , 2500, function(){
+                $("#et-sns-slide").append( $("#et-sns-slide").children().first() );
+                $("#et-sns-slide").css( "top" , "-175px" );
+            });
+        }
+
+        $("#et-sns-slide >div").hover(
+            function(){
+                clearInterval( sSlide );
+                $(this).children(".et-info").fadeIn().css("display" , "flex");
+            }, 
+            function(){
+                sSlide = setInterval ( snsSlide , 2800 );
+                $(".et-info").fadeOut();
+            });
+    } // 1280px보다 작거나 같을 때
 
 });//All END
